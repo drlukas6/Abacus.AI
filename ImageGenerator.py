3a26baa12895688ddc474a72d6552598a921a116
drlukas6 <lsestic@gmail.com>
2019-03-17 17:27:10 +0100

    New Fonts

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -17,13 +17,15 @@ class ImageGenerator:
         if not os.path.exists(symbols_folder):
             os.mkdir(symbols_folder)
         self.available_fonts = os.listdir(self.fonts_folder)
-        self.symbols = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/'}
+        self.symbols = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', ':'}
+
 
     def print_directory_status(self):
         print('Current directory: {}'.format(self.current_directory))
         print('Backgrounds directory: {}'.format(self.backgrounds_folder))
         print('Fonts directory: {}'.format(self.fonts_folder))
         print('Symbols directory: {}'.format(self.symbols_folder))
+
 
     def generate_text_image(self, font, size, text):
         try:
@@ -36,24 +38,31 @@ class ImageGenerator:
         image_to_draw_on.text((0, 0), text, (0, 0, 0), loaded_font)
         return background_image
 
+
     def generate_examples_for_text(self, text, number_of_examples):
+        fonts_to_use = set()
+        while len(fonts_to_use) < number_of_examples:
+            random_font_index = np.random.randint(0, len(self.available_fonts))
+            random_font_location =  os.path.join(self.fonts_folder, self.available_fonts[random_font_index])
+            if 'DS_Store' not in random_font_location:
+                fonts_to_use.add(random_font_location)
         for i in range(0, number_of_examples):
             symbol_directory = os.path.join(self.symbols_folder, text)
             if not os.path.exists(symbol_directory):
                 os.mkdir(symbol_directory)
             random_font_size = np.random.randint(50, 70)
-            random_font_index = np.random.randint(0, len(self.available_fonts))
-            random_font_location = os.path.join(self.fonts_folder,
-                                            self.available_fonts[random_font_index])
-            generated_image = self.generate_text_image(random_font_location,
-                                                       random_font_size,
+            generated_image = self.generate_text_image(font=fonts_to_use.pop(),
+                                                       size=random_font_size,
                                                        text=text)
-            image_path = os.path.join(symbol_directory, '{}_{}.png'.format(text, i))
+            image_path = os.path.join(symbol_directory, 'Symbol_{}_{}.png'.format(text, i))
             generated_image.save(image_path, format='PNG')
+
 
     def generate_examples_for_all_symbols(self, number_of_examples):
         for symbol in self.symbols:
+            print('Processing symbol: ', symbol, ' ...')
             self.generate_examples_for_text(symbol, number_of_examples)
+
 
     def combine_images(self, smaller_image_location, background_image_location):
         smaller_image = cv2.imread(smaller_image_location, -1)
--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ image_generator.print_directory_status()
 
 # image_generator.generate_text_image(os.path.join(image_generator.fonts_folder, 'FilamentFive.otf'), 40, 'LUKAS')
 
-image_generator.generate_examples_for_all_symbols(5)
+image_generator.generate_examples_for_all_symbols(15)
 
 # image_generator.combine_images(smaller_image_location=os.path.join(image_generator.current_directory,
 #                                                                    't.png'),
