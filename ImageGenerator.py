33e059026663114ff7fa46b6e57ebe75f4d49e52
drlukas6 <lsestic@gmail.com>
2019-03-17 16:58:02 +0100

    Unused imports

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+
 class ImageGenerator:
     def __init__(self, backgrounds_folder, fonts_folder, symbols_folder):
         self.current_directory = os.getcwd()
@@ -23,6 +23,7 @@ class ImageGenerator:
         print('Current directory: {}'.format(self.current_directory))
         print('Backgrounds directory: {}'.format(self.backgrounds_folder))
         print('Fonts directory: {}'.format(self.fonts_folder))
+        print('Symbols directory: {}'.format(self.symbols_folder))
 
     def generate_text_image(self, font, size, text):
         try:
@@ -57,7 +58,6 @@ class ImageGenerator:
     def combine_images(self, smaller_image_location, background_image_location):
         smaller_image = cv2.imread(smaller_image_location, -1)
         s_height, s_width, s_channels = smaller_image.shape
-        print(smaller_image.shape)
         s_alpha = smaller_image[:, :, 3] / 255.0
         l_alpha = 1.0 - s_alpha
         background_image = cv2.imread(background_image_location)
@@ -66,9 +66,6 @@ class ImageGenerator:
         x_max = x_offset + s_width
         y_offset = np.random.randint(0, (b_height - s_height))
         y_max = y_offset + s_height
-        print('Smaller size: {}*{}'.format(s_width, s_height))
-        print('Background size: {}*{}'.format(b_width, b_height))
-        print('Offset: x{}, y{}'.format(x_offset, y_offset))
         background_copy = background_image.copy()
         for c in range(0, 3):
             background_copy[y_offset:y_max, x_offset:x_max, c] = (s_alpha * smaller_image[:, :, c] +
